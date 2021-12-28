5b7efbc59841917baa02001709c5602845c6ad05
LucasHeitorPirola <lucas_heitor_pirola@hotmail.com>
2021-12-28 17:39:56 -0300

    Aparição do cactus

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
 
 function handleKeyUp(event) {
   if (event.keyCode === 32) {
+    if(!isJumping) {
     jump();
+    }
   }
 }
 
 function jump() {
   let position = 0;
+  isJumping = true;
 
   let upInterval = setInterval(() => {
     if (position >= 150) {
@@ -17,7 +22,7 @@ function jump() {
       let downInterval = setInterval(() => {
         if (position <= 0) {
           clearInterval(downInterval);
-          isJumping = false;
+          isJumping = false
         } else {
           position -= 20;
           dino.style.bottom = position + 'px';
@@ -31,4 +36,14 @@ function jump() {
   }, 20);
 }
 
+function creatCactus() {
+  const cactus = document.createElement('div');
+  let cactusPosition = 1000;
+
+  cactus.classList.add('cactus');
+  background.appendChild(cactus);
+  cactus.style.left = cactusPosition + 'px';
+}
+
+creatCactus();
 document.addEventListener('keyup', handleKeyUp);
